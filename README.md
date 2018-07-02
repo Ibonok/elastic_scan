ea120031f8be9e04e6cacfbb23ca48d40fdcf7bf
Ibonok <mwollner@mworuna.net>
2018-07-02 13:44:54 +0200

    New

---
+++ README.md
@@ -0,0 +1,149 @@
+# elastic_scan
+> Dump Elasticsearch Instances
+
+The code is very dirty but works so far :-)
+
+# Installation
+
+```
+git clone https://github.com/Ibonok/elastic_scan.git
+cd elastic_scan
+pip3 install -r requirements.txt
+```
+
+> Donate: (ETH) 0x489B56bA505F88a054893d5BdE2c8b35f4A33FAb
+
+<p align="center">
+  <img width="800" src="https://cdn.rawgit.com/ibonok/elastic_scan/7b553ae/elastic_scan.svg">
+</p>
+
+# Usage
+
+```
+python3 elastic_scan.py
+
+usage: elastic.py [-h] [-t [TIMEOUT]] [-r [RETRIES]] [-s [SIZE]]
+                  [-v [VERBOSE]] [-i [INDEXES [INDEXES ...]]] [-d [DUMP]]
+                  [-o {csv,json}] [-std [STDOUT]] [--ip [IP]] [-f [FILENAME]]
+
+Search for elasticsearch on the Internet. Display all Indexes and
+dump the Indexes.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -t [TIMEOUT], --timeout [TIMEOUT]
+                        Connection Timeout, Default = 30s
+  -r [RETRIES], --retries [RETRIES]
+                        Connection Retries, Default = 0
+  -s [SIZE], --size [SIZE]
+                        Dump Entries, Default = 1
+  -v [VERBOSE], --verbose [VERBOSE]
+                        Increase verbosity level 0:INFO, 1:DEBUG, 2:WARNING,
+                        3:ERROR, 4:CRITICAL
+  -i [INDEXES [INDEXES ...]], --indexes [INDEXES [INDEXES ...]]
+                        Give known indexes : index1 index2 indexn, Default = *
+  -d [DUMP], --dump [DUMP]
+                        Dump indexes of target. Default = False
+  -o {csv,json}, --output {csv,json}
+                        Output File: ip-indexname, csv=only _source, json=all
+  -std [STDOUT], --stdout [STDOUT]
+                        Display DUMP to stdout, Default = True
+  --ip [IP]             Target IP:PORT
+  -f [FILENAME], --filename [FILENAME]
+                        File with IP:PORT
+```
+
+# Example
+> Get all indexes
+
+```
+➜  ~ python3 elastic_scan.py --ip 127.0.0.1:9200                  
+##################################################
+	Connection Timeout: 30
+	Connection Retries: False
+	Scroll Size: 1
+	Indexes: *
+	Dump Elasticsearch Host: False
+	Output Format: None
+##################################################
+Connect to  127.0.0.1:9200
+Name: AOw7Lql
+Clustername: elasticsearch
+Lucene Version: 7.2.1
+Try to get INDEXES
+Index: pastehunter-2018-26
+Index: pastehunter-2018-24
+Index: .triggered_watches
+Index: .watches
+Index: .kibana
+Index: pastehunter-2018-25
+Index: pastehunter-2018-27
+Index: .monitoring-es-6-2018.06.14
+Index: .monitoring-alerts-6
+```
+
+> Dump the last 3 entries of all Indexes
+```
+python3 elastic_scan.py --ip 127.0.0.1:9200 -d -s 3 --out csv
+##################################################
+	Connection Timeout: 30
+	Connection Retries: False
+	Scroll Size: 3
+	Indexes: *
+	Dump Elasticsearch Host: True
+	Output Format: csv
+##################################################
+Connect to  127.0.0.1:9200
+Name: AOw7Lql
+Clustername: elasticsearch
+Lucene Version: 7.2.1
+Try to get INDEXES
+Index: .monitoring-es-6-2018.06.14
+Output to CSV IP/filename: 127.0.0.1:9200/.monitoring-es-6-2018.06.14.csv
+Index: pastehunter-2018-25
+Output to CSV IP/filename: 127.0.0.1:9200/pastehunter-2018-25.csv
+Index: pastehunter-2018-27
+Output to CSV IP/filename: 127.0.0.1:9200/pastehunter-2018-27.csv
+Index: pastehunter-2018-26
+Output to CSV IP/filename: 127.0.0.1:9200/pastehunter-2018-26.csv
+Index: .watches
+Output to CSV IP/filename: 127.0.0.1:9200/.watches.csv
+Index: .kibana
+Output to CSV IP/filename: 127.0.0.1:9200/.kibana.csv
+Index: pastehunter-2018-24
+Output to CSV IP/filename: 127.0.0.1:9200/pastehunter-2018-24.csv
+Index: .triggered_watches
+Output to CSV IP/filename: 127.0.0.1:9200/.triggered_watches.csv
+Index: .monitoring-alerts-6
+Output to CSV IP/filename: 127.0.0.1:9200/.monitoring-alerts-6.csv
+
+
+➜  ~ ls
+127.0.0.1:9200  elastic_scan.py
+
+```
+
+> Get some Elasticsearch Hosts from shodan
+
+```
+shodan download elasticsearch product:"Elastic" --limit 2
+
+Search query:			product:Elastic
+Total number of results:	42079
+Query credits left:		95
+Output file:			elasticsearch.json.gz
+  [####################################]  100%             
+Saved 1000 results into file elasticsearch.json.gz
+
+shodan parse --fields ip_str,port --separator : elasticsearch.json.gz | sed s'/.$//' > el_ip
+
+python3 elastic_scan.py -f el_ip -d -std
+python3 elastic_scan.py --ip xxx.xxx.xxx.xxx:80 -s -std
+
+file: el_ip
+xxx.xxx.xxx.xxx:80
+xxx.xxx.xxx.xxx:80
+xxx.xxx.xxx.xxx:80
+xxx.xxx.xxx.xxx:80
+
+```
---
+++ elastic_scan.py
@@ -0,0 +1,250 @@
+#!/usr/bin/python3
+############
+# @Author Ibonok
+#
+# Dump Elasticsearch instances
+#
+# Do not use this in productiv enviroments.
+# For educational use only. 
+# 
+############ 
+
+from elasticsearch import Elasticsearch, ElasticsearchException, TransportError
+from colorama import init, Fore, Style
+from datetime import datetime
+
+import os, errno
+import argparse
+import jsbeautifier
+import urllib3
+import re
+import sys
+import logging
+import json
+
+def set_debug_level( level):
+    init(autoreset=True)
+    chooser = { 
+            0: logging.INFO,
+            1: logging.DEBUG,
+            2: logging.WARNING,
+            3: logging.ERROR,
+            4: logging.CRITICAL
+            }
+
+    if level is not None:
+        logger = logging.getLogger()
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter( Fore.GREEN + '%(asctime)s ' + Fore.BLUE + '%(name) -12s ' + Fore.RED + '%(levelname) -8s ' + Style.RESET_ALL + '%(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(chooser.get( level))
+
+def check_args ():
+    init(autoreset=True)
+    global CONNECTION_TIMEOUT
+    global CONNECTION_RETRIES
+    global SIZE
+    global SINGLE_IP
+    global INDEXES
+    global DUMP_INDEXES
+    global OUTPUT
+    global STDOUT
+
+    pars = argparse.ArgumentParser(description=Fore.GREEN + Style.BRIGHT + 'Search for elasticsearch on the Internet. \nDisplay all Indexes and dump the Indexes.' + Style.RESET_ALL)
+
+    pars.add_argument('-t', '--timeout', nargs='?', type=int, default=30, help='Connection Timeout, Default = 30s')
+    pars.add_argument('-r', '--retries', nargs='?', type=int, default=False, help='Connection Retries, Default = 0')
+    pars.add_argument('-s', '--size', nargs='?', type=int, default=1, help='Dump Entries, Default = 1')
+    pars.add_argument('-v', '--verbose', type=int, nargs='?', help='Increase verbosity level 0:INFO, 1:DEBUG, 2:WARNING, 3:ERROR, 4:CRITICAL', default=None)
+    pars.add_argument('-i', '--indexes', type=str, nargs='*', help='Give known indexes : index1 index2 indexn, Default = *', default='*')
+    pars.add_argument('-d', '--dump', type=bool, nargs='?', help='Dump indexes of target. Default = False', default=False, const=True)
+    pars.add_argument('-o', '--output', type=str, choices= ('csv', 'json'), help='Output File: ip-indexname, csv=only _source, json=all', default=None)
+    pars.add_argument('-std', '--stdout', type=bool, nargs='?', help='Display DUMP to stdout, Default = False', default=False, const=True)
+
+    pars.add_argument('--ip', nargs='?', help='Target IP:PORT')
+    pars.add_argument('-f', '--filename', nargs='?', help='File with IP:PORT')
+
+    args = pars.parse_args()
+
+    CONNECTION_TIMEOUT = args.timeout
+    CONNECTION_RETRIES = args.retries
+    SIZE = args.size
+    INDEXES = args.indexes
+    DUMP_INDEXES = args.dump
+    OUTPUT = args.output
+    STDOUT = args.stdout
+    set_debug_level ( args.verbose)
+
+    print_settings()
+
+    if args.ip is None and args.filename is None:
+        pars.error(Fore.RED + '-f/--filename or --ip required')
+    elif args.ip and args.filename is None: 
+        return args.ip, True
+    elif args.ip is None and args.filename: 
+        return args.filename, False
+    elif args.ip and args.filename: 
+        pars.error(Fore.RED + 'To many Parameters, please choose -f/--filename or --ip')
+
+
+def print_settings():
+    print (Fore.RED + '#' * 50)
+    print ('\t' + Fore.GREEN + 'Connection Timeout: ' + Fore.BLUE + str(CONNECTION_TIMEOUT))
+    print ('\t' + Fore.GREEN + 'Connection Retries: ' + Fore.BLUE + str(CONNECTION_RETRIES))
+    print ('\t' + Fore.GREEN + 'Scroll Size: ' + Fore.BLUE + str(SIZE))
+    print ('\t' + Fore.GREEN + 'Indexes: ' + Fore.BLUE + str(INDEXES))
+
+    print ('\t' + Fore.GREEN + 'Dump Elasticsearch Host: ' + Fore.BLUE + str(DUMP_INDEXES))
+    print ('\t' + Fore.GREEN + 'Output Format: ' + Fore.BLUE + str(OUTPUT))
+    print (Fore.RED + '#' * 50)
+
+
+def get_indexes ( es, ip):
+    try:
+        print (Fore.BLUE + 'Try to get INDEXES')
+        for index in es.indices.get(INDEXES):
+            print (Fore.GREEN + 'Index: ' + Fore.RED + index)
+            if DUMP_INDEXES:
+                dump_index (es, index, ip)
+        print ('\n')
+    except ElasticsearchException as e:
+        print ('Error: ', e)
+    except TransportError as e:
+        print ('Error: ', e)
+
+def dump_index ( es, this_index, ip):
+    page = es.search(
+        index = this_index,
+        doc_type = '',
+        scroll = '1m',
+        size = SIZE,
+        request_timeout = 10,
+        body = {
+    })
+    sid = page['_scroll_id']
+
+    #scroll_size = page['hits']['total']
+    oth = True
+    page = es.scroll(scroll_id = sid, scroll = '1m')
+    if STDOUT and OUTPUT is None:
+        print (str(page))
+        #for hit in page['hits']['hits']:
+        #    v = hit["_source"]
+        #    data = json.dumps(v)
+        #    print (jsbeautifier.beautify(data))
+    elif OUTPUT == 'csv':
+        print (Fore.RED + 'Output to CSV IP/filename: ' + Fore.BLUE + ip + Fore.GREEN + '/' + this_index + '.csv')
+        for hit in page['hits']['hits']:
+            v = hit["_source"]
+            data = json.dumps(v)
+            data = json.loads(data)
+            write_csv(data, this_index, ip, oth)
+    elif OUTPUT == 'json':
+        print (Fore.RED + 'Output to IP/JSON filename: ' + Fore.BLUE + ip + Fore.GREEN + '/' + this_index + '.json')
+        write_json(page, this_index, ip)
+
+def write_json(data, this_index, ip):
+    try: 
+        os.makedirs(ip)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            print (Fore.RED + 'Cannot create directory')
+            raise
+    
+    try: 
+        datei = open(ip + '/' + this_index + '.json', 'a')
+        datei.write(jsbeautifier.beautify(str(data)))
+        datei.close()
+    except FileNotFoundError:
+        print(Fore.RED + 'Input file not found!')
+
+def write_csv(data, this_index, ip, oth):
+    try: 
+        os.makedirs(ip)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            print (Fore.RED + 'Cannot create directory')
+            raise
+
+    try:
+        datei = open(ip + '/' + this_index + '.csv', 'a')
+        headers = ''
+        values = ''
+        for key, value in data.items():
+            headers += str(key) + ','
+            values += str(value) + ','
+        if SIZE > 1 and oth:
+            oth = False
+            datei.write(headers[:-1] + '\n')
+        datei.write(values[:-1])
+        datei.close()
+    except FileNotFoundError:
+        print(Fore.RED + 'Input file not found!')
+
+def check_ipport(ip):
+    if re.findall('^(?:[0-9]{1,3}\.){3}[0-9]{1,3}:[\d]{1,4}$', ip):
+        return True
+    else:
+        print ('Not an ip:port, ', ip)
+        return False
+        
+
+def test4elastic(ip):
+    if check_ipport(ip):
+        ipport = ip.split(':')
+        try:
+            http = urllib3.PoolManager(timeout=CONNECTION_TIMEOUT, retries=CONNECTION_RETRIES, port=int(ipport[1]))
+            check_el = http.request('GET', 'http://' + ip.rstrip())
+            if re.search (r'lucene', str(check_el.data)):
+                return True
+            else:
+                print ('Not an Elastic Search Instanz')
+                return False
+        except urllib3.exceptions.ConnectTimeoutError:
+            print (Fore.RED + Style.BRIGHT + 'Error: Connection Timeout')
+            return False
+    else:
+        return (Fore.RED + Style.BRIGHT + 'Error: Go Next')
+
+
+def create_connection(ip):
+    print (Fore.GREEN + 'Connect to ', ip)
+    es = Elasticsearch([ip])
+    version = es.info()
+    print (Fore.BLUE + 'Name: ' + version['name'] + '\nClustername: ' + version['cluster_name'] + '\nLucene Version: ' + version['version']['lucene_version'])
+    if version['version']['lucene_version'].startswith('4'):
+        print (Fore.RED + 'Wrong elasticsearch version...')
+        return None
+    else:
+        return es
+
+
+def single_ip(ip):
+    if test4elastic(ip):
+        es = create_connection(ip)
+        if es is not None:
+            get_indexes ( es, ip)
+    else:
+        print ('Error got next')
+
+
+def input_file(filename):
+    file = open (filename, 'r')
+    for ip in file:
+        if test4elastic(ip.rstrip()):
+            es = create_connection(ip.rstrip())
+            if es is not None:
+                get_indexes (es, ip.rstrip())
+        else:
+            print ('Error go next')
+
+
+try:
+    (value, typ) = check_args()
+    if typ:
+        single_ip(value)
+    else:
+        input_file(value)
+except KeyboardInterrupt:
+    sys.exit()
